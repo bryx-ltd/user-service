3d4d6e1d628a397a14e5a97a1ec0c1c7028aae17
Neil Davies <neil.davies@thrivelearning.com>
2025-04-23 17:53:23 +0100

    return jwt as cookie

--- login.go
+++ login.go
@@ -58,7 +58,7 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"token": tokenString,
-	})
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600, "/", "", false, true)
+	c.JSON(http.StatusOK, gin.H{})
 }
